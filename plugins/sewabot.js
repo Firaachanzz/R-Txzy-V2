0643360e305179026ee9e485e6cd11085d5ec7a3
Firaachanzz <97344921+Firaachanzz@users.noreply.github.com>
2022-02-11 08:45:10 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,8 +11,8 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 083830815715
+GoPay: Gakda bg
 `.trim()
     const button = {
         buttonText: 'List Harga',
