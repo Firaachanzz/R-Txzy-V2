fd3cf3aa50fd2037329053b295116b1715660da8
Firaachanzz <97344921+Firaachanzz@users.noreply.github.com>
2022-02-14 18:42:58 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,12 +1,12 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
-  let name = '𝐟𝐢𝐫𝐚𝐚'
+  let name = 'RII'
   number = owner[0].replace(/[^0-9]/g, '')
   let njid = number + '@s.whatsapp.net'
   let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
 
-  let name2 = '𝐟𝐢𝐫𝐚𝐚'
+  let name2 = 'RII'
   number2 = owner[1].replace(/[^0-9]/g, '')
   let njid2 = number2 + '@s.whatsapp.net'
   let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
